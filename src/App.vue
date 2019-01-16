d8cf71647c133e6f89229cdd702206da279ea68d
M <M@qq.com>
2019-01-17 00:45:50 +0800

    init commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <clock></clock>
+    <nock></nock>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import VueClock from 'vue-clock'
+
+const { Clock, Nock } = VueClock
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    Clock,
+    Nock
   }
 }
 </script>
